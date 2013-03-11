5464475183a9a757a7e811d244f23035ac5eba54
Benjamin Woodruff <benjaminwoodruff@ufl.edu>
2013-03-11 00:29:14 -0400

    Add convenience function for list parsing
    Part of the `parseE` definition requires parsing a list of arguments. A
    helper function, `many` should be reusable in other areas of the parser.
    
    There is now a `parsePlaceholder` function that will take a
    `placeholder` identifier and replace it with an `AstDummy` node. This is
    useful for testing, and actually allows us to generate a few basic trees
    from inputs (which appears to work!).

--- Parser.hs
+++ Parser.hs
@@ -18,10 +18,11 @@ instance Parsable Token where
 
 -- Define the Parser Monad
 newtype Parser a = Parser a
-    -- Use the values for `a` as defined in ParserResult and ParserConsumed
+    -- Use the values for `a` as defined below
     deriving Show
 
 type ParserResult = Parser (Ast, [Token])
+type ParserResultList = Parser ([Ast], [Token])
 type ParserConsumed = Parser [Token]
 
 instance Monad Parser where
@@ -36,6 +37,17 @@ consume (tHead:tTail) matched
                                  ++ "' with '" ++ (show tHead) ++ "'"
 consume _ matched = error $ "Expecting '" ++ (show matched) ++ "' but got EOF"
 
+-- Repeatedly call a parser until a end conditional is matched
+many :: [Token] -> ([Token] -> ParserResult) -> ([Token] -> Bool)
+        -> ParserResultList
+many ta par cond =
+    if cond ta then return ([], ta)
+    else do
+        (ab, tb) <- par ta
+        (ac, tc) <- many tb par cond
+        return (ab : ac, tc)
+
+-- Generate error handler when a matcher fails
 notFound :: String -> [Token] -> b
 notFound expected (token:_) =
     error $ "Expected " ++ expected ++ " but instead found (" ++ (show token)
@@ -45,12 +57,35 @@ notFound expected _ =
 
 -- Parsers for each grammar component
 parseE :: [Token] -> ParserResult
+
+-- E -> 'let' D 'in' E
 parseE (TokenIdentifier "let" : tokens) = do
     (aa, ta) <- parseD tokens
     tb <- consume ta $ TokenIdentifier "in"
     (ac, tc) <- parseE tb
     return (AstLet aa ac, tc)
-parseE t = notFound "expression" t
+
+-- E -> 'fn' Vb+ '.' E
+parseE (TokenIdentifier "fn" : tokens) = do
+    (aa, ta) <- parseVb tokens -- There should be at least one Vb
+    (ab, tb) <- many ta parseVb (\t -> (head t) == (TokenOperator "."))
+    tc <- consume tb $ TokenOperator "."
+    (ad, td) <- parseE tc
+    return (AstLambda (aa : ab) ad, td)
+
+parseE t = parseEw t
+
+-- Stuff below is not yet implemented
+parsePlaceholder :: [Token] -> ParserResult
+parsePlaceholder (TokenIdentifier "placeholder" : tokens) =
+    return (AstDummy, tokens)
+parsePlaceholder t = notFound "placeholder" t
+
+parseEw :: [Token] -> ParserResult
+parseEw = parsePlaceholder
 
 parseD :: [Token] -> ParserResult
-parseD = parseE
+parseD = parsePlaceholder
+
+parseVb :: [Token] -> ParserResult
+parseVb = parsePlaceholder
